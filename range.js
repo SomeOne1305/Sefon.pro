62e748d1ad73755aa5ed548b9ee21905c5e4709d
Akobir5 <toshtemirovakobir8@gmail.com>
2022-08-24 11:54:40 +0500

    voicecontroller is finished

--- range.js
+++ range.js
@@ -1,7 +1,9 @@
 let input = document.querySelector('#range');
 input.addEventListener('input', ()=>{
     console.log(input.value / 100);
+    audio.volume = input.value / 100
 })
+
 $( '.slider' ).on( 'input', function( ) {
 $( this ).css( 'background', 'linear-gradient(to right, #ffde01 0%, #ffde01 '+this.value +'%, #7398c5 ' + this.value + '%, #7398c5 100%)' );
 } );
