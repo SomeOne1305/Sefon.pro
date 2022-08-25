1c6e4f752f0ed5609d3103ad72ed7ce61e4d91da
Akobir5 <toshtemirovakobir8@gmail.com>
2022-08-25 11:30:26 +0500

    Mplayer

--- js/app.js
+++ js/app.js
@@ -8,43 +8,96 @@ window.addEventListener('scroll', ()=>{
         document.querySelector('.MainBlock').style.marginTop = "0"
     }
 })
+
+let musicSources = [
+    './musics/music1.mp3',
+    './musics/music2.mp3'
+];
+
 let icon = document.querySelectorAll('.playOrPause i.fa');
 let btns = document.querySelectorAll('.playOrPause');
 let audio = document.querySelector('.MusicPlayer audio')
 let like = document.querySelector('.like')
 
-btns.forEach( async (item, index)=>{
-    let source = item.getAttribute('data-music-name');
-    item.addEventListener('click',()=>{
-    //     function playPauseMusic(){
-        audio.setAttribute('src', source);
-    //    }
-    //    playPauseMusic()
- 
-    if (icon[index].classList.contains('fa-play')) {
+// btns.forEach( async (item, index)=>{
+//     let source = item.getAttribute('data-music-name');
+//     item.addEventListener('click',()=>{
+//     //     function playPauseMusic(){
+//         audio.setAttribute('src', source);
+//     //    }
+//     //    playPauseMusic()
+//     document.querySelector('.fullDuration').innerHTML = Math.floor(audio.duration)
+//     if (icon[index].classList.contains('fa-play')) {
+//         btns.forEach((item, index) =>{
+//             icon[index].classList.remove('fa-pause')
+//             icon[index].classList.add('fa-play')
+//             audio.pause()
+//         })
+//         icon[index].classList.add('fa-pause');
+//         icon[index].classList.remove('fa-play');
+//         document.querySelector('.pauseAndplay i.fa').classList.remove('fa-play')
+//         document.querySelector('.pauseAndplay i.fa').classList.add('fa-pause')
+//         audio.play()
+      
+//     } else{
+//             icon[index].classList.remove('fa-pause')
+//             icon[index].classList.add('fa-play');
+//             document.querySelector('.pauseAndplay i.fa').classList.add('fa-play')
+//             document.querySelector('.pauseAndplay i.fa').classList.remove('fa-pause')
+//             audio.pause()
+//     }
+//     })
+// })
+function playMusic(item, index) {
         btns.forEach((item, index) =>{
             icon[index].classList.remove('fa-pause')
             icon[index].classList.add('fa-play')
-            audio.pause()
+            item.classList.remove('isActive')
+            item.classList.remove('isPause')
         })
         icon[index].classList.add('fa-pause');
         icon[index].classList.remove('fa-play');
         document.querySelector('.pauseAndplay i.fa').classList.remove('fa-play')
         document.querySelector('.pauseAndplay i.fa').classList.add('fa-pause')
-        document.querySelector('.fullDuration').innerHTML = Math.floor(audio.duration)
         audio.play()
-      
-    } else{
+        item.classList.add('isActive')
+        
+}
+function pauseMusic(item, index) {
             icon[index].classList.remove('fa-pause')
             icon[index].classList.add('fa-play');
             document.querySelector('.pauseAndplay i.fa').classList.add('fa-play')
             document.querySelector('.pauseAndplay i.fa').classList.remove('fa-pause')
             audio.pause()
-    }
+            item.classList.add('isPause')
+}
+btns.forEach((item, index)=>{
+   
+    item.addEventListener('click', ()=>{
+        if (item.querySelector('i.fa').classList.contains('fa-play')) {
+            
+            if(item.classList.contains('isActive')){
+                
+                if (item.classList.contains('isPause')) {
+                    playMusic(item, index)
+                    
+                }else{
+                    pauseMusic(item, index)
+                }
+            }else{
+                audio.src = item.getAttribute('data-music-name')
+                playMusic(item, index)
+            }
+        }else{
+            pauseMusic(item, index)
+        }
     })
+
+    
 })
 document.querySelector('.pauseAndplay').addEventListener('click', ()=>{
 })
+
 
 let likeBtn = document.querySelectorAll('.like i');
 let likeNumber = document.querySelectorAll('.like span');
@@ -64,8 +117,14 @@ likeBtn.forEach((item, index)=>{
 // document.querySelector('.volume').addEventListener('click', setProgress)
 
 let hearts = document.querySelectorAll('.like');
+let dislike = document.querySelectorAll('.dislike');
+let span1 = document.querySelectorAll('.dislike span');
 let span = document.querySelectorAll('.like span');
 span.forEach(item=>{
+   item.innerHTML = 0;
+
+})
+span1.forEach(item=>{
    item.innerHTML = 0;
 
 })
@@ -78,3 +137,12 @@ hearts.forEach((item)=>{
         item.querySelector('span').innerHTML = +(number + 1)
     })
 })
+dislike.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        item.querySelector('i').style.webkitTextStroke = "2px #FC5D5D"
+        let number = 0;
+        item.querySelector('span').innerHTML = number;
+        item.querySelector('span').innerHTML = +(number + 1)
+    })
+})
+
